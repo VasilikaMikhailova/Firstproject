633679a0ae82cdd83ce52ea2a3e5096ab6c84236
Vasilika <vasimikon@gmail.com>
2022-10-25 06:44:36 +0300

    Обновления дневника 25.10.2022

--- JS/script1.js
+++ JS/script1.js
@@ -1,45 +1,11 @@
 "use strict";
 
-// let a; // объявляем переменную
+// const htmlElement = document.documentElement;
+// const headElement = document.head;
+// const bodyElement = document.body;
+// console.log(htmlElement);
+// console.log(headElement);
+// console.log(bodyElement);
 
-// a = 1; // записываем в нее значение 1
-// alert(a); // выведет 1
-
-// a = 2; // записываем теперь значение 2, затирая значение 1
-// alert(a); // выведет 2
-
-// let name1 = prompt("Ваше имя?");
-// alert("Ваше имя: " + name1);
-
-// alert("Ваш возраст: " + prompt("Ваш возраст?"));
-
-// let num1 = prompt("Введите первое число");
-// let num2 = prompt("Введите второе число");
-
-// alert(Number(num1) + Number(num2));
-
-//alert("Сторона квадрата: " + prompt(Number("Сторона квадрата" ** 2)));
-
-// let num1 = prompt("Введите сторону квадрата");
-// alert(Number(num1) ** 2);
-
-// let num1 = prompt("Введите одну сторону прямоугольника");
-// let num2 = prompt("Введите вторую сторону прямоугольника");
-// alert("Площадь прямоугольника равна " + Number(num1) * Number(num2));
-
-// document.write(123);
-// document.write("text");
-
-// document.write("<i>text</i>");
-
-// document.write("1<br>");
-// document.write("2<br>");
-// document.write("3<br>");
-// document.write("4<br>");
-// document.write("5<br>");
-
-// function func(str) {
-//   return str;
-// }
-
-// alert(func);
+let container = document.querySelectorAll(".li");
+console.log(container);
